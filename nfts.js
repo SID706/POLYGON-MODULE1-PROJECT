1e95e61b0a823c51ec8b923b35a712ea0a0fcc62
SID706 <108327406+SID706@users.noreply.github.com>
2023-08-31 22:33:55 +0530

    Create nfts.js

---
+++ nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+    {
+        name: '5',
+        description: 'A robot made from Modular synthesizers. in the style of 90's vintage anime, surrealism, akira. anime line art.A robot made from Modular synthesizers. in the style of 90's vintage anime, surrealism, akira. anime line art.'
+        image: 'QmQNTsvLd6TukZk37gpD42PHRru2qTXNbzgTkpWo1hJ9Wa'
+    },
+    {
+        name: '1',
+        description: 'A robot made from Modular synthesizers. in the style of 90's vintage anime, surrealism, akira. anime line art.A robot made from Modular synthesizers. in the style of 90's vintage anime, surrealism, akira. anime line art.',
+        image: 'QmdwyGupbuWF5ag7BQWfTpLYny3i8TEoZeUZmgUHuyDdhL'
+    },//change
+    {
+        name: '2',
+        description: 'A robot made from Modular synthesizers. in the style of 90's vintage anime, surrealism, akira. anime line art.A robot made from Modular synthesizers. in the style of 90's vintage anime, surrealism, akira. anime line art.',
+        image: 'QmWWMo6wcC1kCLoybfWPm1K16FoSxWFiaHVfGLGeUY6Pon'
+    },
+    {
+        name: '3',
+        description: 'A robot made from Modular synthesizers. in the style of 90's vintage anime, surrealism, akira. anime line art.A robot made from Modular synthesizers. in the style of 90's vintage anime, surrealism, akira. anime line art.',
+        image: 'QmRRutC5EPCLbja23ggjKuWEK2LqF9AjPY6TD5ktHfKpFW'
+    },
+    {
+        name: '4',
+        description: 'A robot made from Modular synthesizers. in the style of 90's vintage anime, surrealism, akira. anime line art.A robot made from Modular synthesizers. in the style of 90's vintage anime, surrealism, akira. anime line art.',
+        image: 'QmWBfEzuWJ7zW6xoJbr3Zp6iUG2iXiZ7fmqzCFVjLJJSD5'
+    },    
+];
+
+module.exports = nfts;
